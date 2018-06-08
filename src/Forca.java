6eba2dc07c4851ad1d594b0011d6d26edbb23c55
Hiago <hiagobcomitre@gmail.com>
2018-06-08 11:15:51 -0300

    Tela de login e modificacao na classe forca

--- src/Forca.java
+++ src/Forca.java
@@ -1,12 +1,16 @@
 import javax.swing.JFrame;
 
 import Tela.Inicio;
+import Tela.Login;
 
 public class Forca {
 
 	public void rodarPrograma() {
 		Inicio tela1 = new Inicio();
 		tela1.Inicio();
+		
+		//Login tela2 = new Login();
+		//tela2.Login();
 		
 	}
 
