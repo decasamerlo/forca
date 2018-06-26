07d3f6b46088033f1a0562062b252b88bde70c68
AndreMerlo <casagrandamerlo@gmail.com>
2018-06-26 14:06:53 -0300

    wip

--- src/controle/JogoC.java
+++ src/controle/JogoC.java
@@ -1,11 +1,22 @@
 package controle;
 
 import tela.JogoT;
+
+import java.awt.Container;
+
 import main.Forca;
 
 public class JogoC {
 
 	private Forca f;
 	private JogoT tela;
+	
+	public JogoC(Forca f) {
+		this.f = f;
+	}
+
+	public Container configuraTela() {
+		return null;
+	}
 	
 }
