ce7212dd379d06c590c6f1ae68b2b3e71c092903
AndreMerlo <casagrandamerlo@gmail.com>
2018-06-26 14:04:16 -0300

    merge

--- src/controle/JogoC.java
+++ src/controle/JogoC.java
@@ -1,28 +1,5 @@
 package controle;
 
-<<<<<<< HEAD
-import javax.swing.JPanel;
-
-import main.Forca;
-import tela.JogoT;
-
-public class JogoC {
-	private Forca f;
-	private JogoT tela;
-	
-	public JogoC(Forca f) {
-		this.f = f;
-		tela = new JogoT(this);
-	}
-	
-	public JPanel configuraTela() {
-		return tela.criaTela();
-	}
-	
-	public void sair() {
-		f.getJanela().dispose();
-	}
-=======
 import tela.JogoT;
 import main.Forca;
 
@@ -30,6 +7,5 @@ public class JogoC {
 
 	private Forca f;
 	private JogoT tela;
->>>>>>> refs/remotes/origin/master
 	
 }
