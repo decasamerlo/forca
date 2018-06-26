3fa3222144873dffc8f84a03c3d3e04fd6e12f12
AndreMerlo <casagrandamerlo@gmail.com>
2018-06-26 14:04:57 -0300

    merge

--- src/tela/JogoT.java
+++ src/tela/JogoT.java
@@ -1,90 +1,5 @@
 package tela;
 
-<<<<<<< HEAD
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import controle.JogoC;
-
-public class JogoT {
-
-	private static final int borda = 10;
-	JPanel telaJogo;
-	JPanel telaPalavra;
-	JPanel telaForca;
-	JPanel teclado;
-	JogoC cntJogo;
-
-	public JogoT(JogoC cntJogo) {
-		this.cntJogo = cntJogo;
-		
-		telaJogo = new JPanel(null);
-		telaPalavra = criaTelaPalavra();
-		telaForca = criaTelaForca();
-		teclado = criaTeclado();
-		
-		telaJogo.add(telaPalavra);
-		telaJogo.add(telaForca);
-		telaJogo.add(teclado);
-
-	}
-
-
-	private JPanel criaTelaPalavra() {
-		JPanel panel = new JPanel(null);
-		panel.setBounds(borda, borda, telaJogo.getWidth()-borda, borda);
-		panel.setBackground(Color.black);
-		return panel;
-	}
-
-	private JPanel criaTelaForca() {
-		JPanel panel = new JPanel(null);
-		panel.setBounds(borda, telaPalavra.getHeight()+borda, telaJogo.getWidth()-borda, borda);
-		panel.setBackground(Color.blue);
-		return panel;
-	}
-
-	private JPanel criaTeclado() {
-		JPanel panel = new JPanel(null);
-		panel.setBounds(borda, telaForca.getY() + telaForca.getHeight()
-				 + borda, telaJogo.getWidth() - borda * 3, borda * 30);
-		panel.setBackground(Color.YELLOW);
-		return panel;
-	}
-
-	public JPanel getTelaJogo() {
-		return telaJogo;
-	}
-
-	public void setTelaJogo(JPanel telaJogo) {
-		this.telaJogo = telaJogo;
-	}
-
-	public JPanel getTelaForca() {
-		return telaForca;
-	}
-
-	public void setTelaForca(JPanel telaForca) {
-		this.telaForca = telaForca;
-	}
-
-	public JPanel getTeclado() {
-		return teclado;
-	}
-
-	public void setTeclado(JPanel teclado) {
-		this.teclado = teclado;
-	}
-	
-	public JPanel criaTela() {
-		return telaJogo;
-	}
-
-=======
 import java.util.Random;
 
 import javax.swing.ImageIcon;
@@ -153,5 +68,4 @@ public class JogoT {
 
 	//Adicionar lógica de conferência de letra a letra
 	
->>>>>>> refs/remotes/origin/master
 }
